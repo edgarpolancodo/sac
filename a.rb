75348cdf62943a6cd25b261dbeaa67666a4aa7ea
Edgar De Jesus <theboss.edgar14@gmail.com>
2012-06-21 00:42:49 -0400

    Construir conversaciones mas user-friendly

--- a.rb
+++ a.rb
@@ -13,13 +13,15 @@ get '/crear/:cid' do
 	my = Mysql::new("localhost", "root", "asdasd", "sac")
 	res = my.query("select * from Mensajes WHERE ConversacionID = '#{params[:cid]}'")
 	res2 = my.query("select * from Respuestas r, Mensajes m WHERE ConversacionID = '#{params[:cid]}' AND r.MensajeID = m.ID;")
-	erb :index, :locals => {:mensajes => res, :respuestas => res2, :cid => params[:cid]}
+	link = request.url.sub("crear", "responder")	
+	erb :index, :locals => {:mensajes => res, :respuestas => res2, :cid => params[:cid], :link => link}
+	
 end
 
 post '/crear/:cid' do
 	my = Mysql::new("localhost", "root", "asdasd", "sac")
 
-	
+	link = request.url.sub("crear", "responder")
 	mensajeID = "NULL"
 	respuestaID = "NULL"
 	if params[:origen].include? 'm'
@@ -28,7 +30,7 @@ post '/crear/:cid' do
 		respuestaID = params[:origen].split('r').last
 	end
 	my.query("insert into Mensajes(ConversacionID, Mensaje, Tipo_Declaracion, MensajeAnterior, BasadoRespuesta) VALUES "+
-	"('#{params[:cid]}', '#{params[:declaracion]}', '#{params[:tipo]}', '#{mensajeID}', '#{respuestaID}')")
+	"('#{params[:cid]}', '#{params[:declaracion]}', '#{params[:tipo]}', '#{mensajeID}', '#{respuestaID}'); update Mensajes set MensajeAnterior = NULL, BasadoRespuesta = NULL Where MensajeAnterior=0 AND BasadoRespuesta=0;")
 	res = my.query("select ID from Mensajes WHERE Mensaje = '#{params[:declaracion]}' AND Tipo_Declaracion = '#{params[:tipo]}' AND ConversacionID = '#{params[:cid]}'")
 	if params[:tipo] == "FREE"
 		res.each do |row|
@@ -46,7 +48,7 @@ post '/crear/:cid' do
 	end
 	res = my.query("select * from Mensajes WHERE ConversacionID = '#{params[:cid]}'")
 	res2 = my.query("select * from Respuestas r, Mensajes m WHERE ConversacionID = '#{params[:cid]}' AND r.MensajeID = m.ID AND m.Tipo_Declaracion <> 'FREE';")
-	erb :index, :locals => {:mensajes => res, :respuestas => res2, :cid => params[:cid]}
+	erb :index2, :locals => {:mensajes => res, :respuestas => res2, :link => link}
 	
 end
 
